0ac56d1f177e9da5affc937bcdec73f1135a678b
lintrix <lintrix@Lintrix.lintrix.com>
2026-08-07 07:55:09 +0300

    importing bootstrap js lib

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-icons/font/bootstrap-icons.css'
+import 'bootstrap/dist/js/bootstrap.bootstrap.min.js'
 import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
