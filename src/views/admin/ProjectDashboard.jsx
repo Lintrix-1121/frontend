bf44d1f858a131430e2e5b08b4e4b45d4a97df32
lintrix <lintrix@Lintrix.lintrix.com>
2026-08-10 22:02:17 +0300

    Projects func  enhancement

--- src/views/admin/ProjectDashboard.jsx
+++ src/views/admin/ProjectDashboard.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { Container, Row, Col, Card, Button, Table, Badge } from 'react-bootstrap';
-import { useProjectStore } from '../../stores/shared/projectStore';
+import useProjectStore from '../../stores/shared/projectStore';
 import LoadingSpinner from '../../components/admin/LoadingSpinner';
 import ErrorMessage from '../../components/projects/ErrorMessage';
 
